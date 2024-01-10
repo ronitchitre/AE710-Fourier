24b4e939fb42146fc9b165ee9c4707cd54e8b71c
Ronit <73128348+ronitchitre@users.noreply.github.com>
2024-01-10 23:18:40 +0530

    add remaining functions

--- constants.py
+++ constants.py
@@ -11,6 +11,21 @@ def window_Bartlett(k, N):
         return 2*k / (N - 1)
     else:
         return 2 - (2*k / (N-1))
+
+def window_rectwin(k, N):
+    return 1
+    
+def scaling(window_type, N):
+    k_arr = np.arange(0, N, 1)
+    if window_type == "hann":
+        weight_sum = window_Hann(k_arr, N) * window_Hann(k_arr, N)
+        scale = 1 / (weight_sum / N)
+    elif window_type == "haming":
+        weight_sum = window_Hamming(k_arr, N) * window_Hamming(k_arr, N)
+        scale = 1 / (weight_sum / N)
+    else:
+        weight_sum = window_rectwin(k_arr, N) * window_rectwin(k_arr, N)
+        scale = 1 / (weight_sum / N)
     
 def fourier_phase(k, n, N):
     return np.exp(1j*(-2*np.pi*n*k) / N)
--- main.py
+++ main.py
@@ -6,17 +6,24 @@ import constants
 
 def make_blocks(g, N, olp):
     olp_frac = olp*0.01
-    num_blocks = ceil(g.shape[0] / (N - floor(olp*N)))
-    block_array = [A[(i*N - floor(olp_frac*N)*i): (i+1)*N - floor(olp_frac*N)*i] for i in range(num_blocks)]
+    num_blocks = floor((g.shape[0] - floor(olp_frac*N)) / (N - 1*floor(olp_frac*N))) 
+    block_array = [g[(i*N - floor(olp_frac*N)*i): (i+1)*N - floor(olp_frac*N)*i] for i in range(num_blocks)]
     return block_array
 
-def windowing(block_array, w):
-    windowed_block_arr = [w*block for block in block_array]
+def windowing(block_array, w, scaling):
+    windowed_block_arr = w * np.array(block_array) * scaling
     return windowed_block_arr
 
 def DFT(windowed_block_arr, N):
     num_fourier_term = floor((N - 1) / 2)
     k_arr = np.arange(0, N, 1)
     fourier_phases = constants.fourier_phase(k_arr.reshape(-1, 1), np.arange(num_fourier_term), N)
-    result = np.dot(windowed_block_arr, fourier_phases)
-    return result
+    fourier_terms = np.dot(windowed_block_arr, fourier_phases)
+    return fourier_terms
+
+def PSD(fourier_terms, N, fs):
+    xeta = N / fs
+    half_power = xeta * np.sum(abs(fourier_terms)**2, axis=0) / fourier_terms.shape[0]
+    full_psd_scale = 2*np.ones_like(half_power)
+    full_psd_scale[0] = 1
+    return full_psd_scale * half_power
